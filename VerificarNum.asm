04e110fb6379874671bb9a08eed9956f9a4b999a
Gabriela Colem Castelo Borges <73849139+gabrielacolen@users.noreply.github.com>
2023-06-17 15:34:04 -0300

    Update VerificarNum.asm

--- VerificarNum.asm
+++ VerificarNum.asm
@@ -11,23 +11,32 @@
 .globl main
 
 main:
-    lui $t0, 0x1001    # t0 = 0x10010000 (endereço base)
-    lw $s0, 0($t0)     # x = MEM[0]
-    addi $t7, $zero, 0 # flag = 0
-    
-    slt $t1, $s0, 50    # t1 = (x < 50)
-    slt $t2, $s0, 100   # t2 = (x < 100)
-    or $t1, $t1, $t2    # t1 = (x < 50) or (x < 100)
-    slt $t2, $s0, 150   # t2 = (x < 150)
-    slt $t3, $s0, 200   # t3 = (x < 200)
-    or $t2, $t2, $t3    # t2 = (x < 150) or (x < 200)
-    or $t1, $t1, $t2    # t1 = (x < 50) or (x < 100) or (x < 150) or (x < 200)
-    
-    beq $t1, $zero, falso # se (flag == 0) vá para falso
-    addi $t7, $zero, 1    # flag = 1
-    
-falso:
-    sw $t7, 4($t0)     # MEM[4] = flag
-    
+lui $t0, 0x1001		# t0 = 0x10010000 (endereço base)
+lw $s0, 0($t0)		# x = MEM[0]
+addi $t1, $zero, 50	# num = 50
+add $t7, $zero, $zero	# flag = 0
+
+slt $t2, $s0, $t1	# se (x<50) vá para a 2° parte do OR
+beq $t2, 1, orTeste	
+addi $t1, $zero, 100
+slt $t2, $t1, $s0	#se (x>100) vá para a segunda parte do or, se não, termine
+beq $t2, 1, orTeste
+j true
+			
+orTeste:
+addi $t1, $zero, 150
+slt $t2, $s0, $t1	# se (x<150) vá para fim
+beq $t2, 1, fim
+addi $t1, $zero, 200
+slt $t2, $t1, $s0	# se (x>200) vá para fim, se não, termine
+beq $t2, 1, fim
+j true
+
+true:
+addi $t7, $zero, 1	# por fim, flag = 1
+
+fim:
+sw $t7, 4($t0)
+
 .data
 a: .word 125
